9647737d25981ad2a064e569061b187e557e136b
EFREN FRIAS <ing.efrenfrias@gmail.com>
2022-05-11 02:48:02 -0500

    Remove dummy files

--- src/main/resources/import.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO users (name, alias) VALUES('sofia', 'sofis');
-INSERT INTO users (name, alias) VALUES('Jannet', 'jan');
-INSERT INTO users (name, alias) VALUES('Melisa', 'mel');
-INSERT INTO users (name, alias) VALUES('Ivonne', 'von');
-INSERT INTO users (name, alias) VALUES('Jayel', 'jay');
